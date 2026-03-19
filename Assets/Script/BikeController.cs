6a53fe30f5501010b2ab84b98f2d0f7d5ff39841
kevin <20010729487lka@gmail.com>
2026-03-19 22:53:25 +0000

    fix

--- Assets/Script/BikeController.cs
+++ Assets/Script/BikeController.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class BikeController : MonoBehaviour
 {
-    public WheelJoint2D backWheelJoint; // 拖入后轮的关节
+    public WheelJoint2D frontWheelJoint; 
+    public WheelJoint2D backWheelJoint;
     public float speed = -1500f; // 马达速度
     public float torque = 10000f; // 马达扭矩
 
-    void Update()
+
+    private void FixedUpdate()
     {
-        // 获取水平输入（A/D 或 左右键）
         float movement = Input.GetAxisRaw("Horizontal");
-        
-        JointMotor2D motor = backWheelJoint.motor;
-        
-        if (movement != 0) {
-            motor.motorSpeed = movement * speed;
-            motor.maxMotorTorque = torque;
-            backWheelJoint.useMotor = true;
-        } else {
-            backWheelJoint.useMotor = false;
-        }
-        
-        backWheelJoint.motor = motor;
+        // 设置前轮马达
+        JointMotor2D frontMotor = frontWheelJoint.motor;
+        frontMotor.motorSpeed = movement * speed; // 根据输入调整速度
+        frontMotor.maxMotorTorque = torque; // 设置最大扭矩
+        frontWheelJoint.motor = frontMotor; // 应用马达设置
+
+        // 设置后轮马达
+        JointMotor2D backMotor = backWheelJoint.motor;
+        backMotor.motorSpeed = movement * speed; // 根据输入调整速度
+        backMotor.maxMotorTorque = torque; // 设置最大扭矩
+        backWheelJoint.motor = backMotor; // 应用马达设置
     }
 }
